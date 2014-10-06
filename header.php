b5d29fde74d5210564078513981460a1b5c47ed5
Alan Coleman <boomfelled@gmail.com>
2014-10-06 17:32:48 +0100

    Google Analytics script added to header

--- header.php
+++ header.php
@@ -54,6 +54,17 @@ switch ( getpageurl( get_permalink() ) ) {
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
 <?php wp_head(); ?> 
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-37786043-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
 
 <!-- Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
