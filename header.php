eabd51c20283da6e7a9cdbbf1f2c5b266cda00ea
Alan Coleman <boomfelled@gmail.com>
2016-11-22 17:10:46 +0000

    Google tag manager tags added

--- header.php
+++ header.php
@@ -19,6 +19,13 @@
 <!--[if IE 8 ]>    <html class="no-js ie8" <?php language_attributes(); ?>> <![endif]-->
 <!--[if (gte IE 9)|!(IE)]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
 <head>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P5FZBC7');</script>
+<!-- End Google Tag Manager -->
 <title><?php wp_title('&#124;', true, 'right'); ?><?php bloginfo('description'); ?> - Alan Coleman</title>
 <meta charset="<?php bloginfo('charset'); ?>" />
 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
@@ -49,6 +56,10 @@
 <![endif]-->
 </head>
 <body <?php body_class(); ?>>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P5FZBC7"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <a href="https://plus.google.com/+AlanColeman" rel="publisher"></a>                 
 <?php responsive_container(); // before container hook ?>        
 <?php responsive_header(); // before header hook ?>
